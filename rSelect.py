c6dc9e7ad2feec1d4071925359b0b9c54fd6cdb2
hardianlawi <hlawi001@e.ntu.edu.sg>
2017-12-17 23:37:20 +0800

    added rSelect algorithm to choose i-th order statistic in an array

---
+++ rSelect.py
@@ -0,0 +1,39 @@
+from random import randrange
+
+def rSelect(arr, order_statistic):
+    n = len(arr)
+
+    if n == 1:
+        return arr[0]
+
+    pivot = randrange(n)
+
+    arr[0], arr[pivot] = arr[pivot], arr[0]
+
+    i, j = 1, 1
+
+    for j in range(n):
+        if arr[0] > arr[j]:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+
+    arr[i-1], arr[0] = arr[0], arr[i-1]
+
+    pivot = i-1
+
+    if pivot + 1 == order_statistic:
+        return arr[pivot]
+    elif pivot + 1 > order_statistic:
+        return rSelect(arr[:pivot], order_statistic)
+    else:
+        return rSelect(arr[pivot+1:], order_statistic - pivot - 1)
+
+
+if __name__ == "__main__":
+
+    print("Input the array:")
+    arr = list(map(int, input().split()))
+    order_statistic = int(input("Input order statistic:\n"))
+
+    print("")
+    print(rSelect(arr, order_statistic))
